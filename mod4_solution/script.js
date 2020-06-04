f9328acd2af0d45c0a26ef8e8e1c4b3454db9dd4
ElizabathJaison <65842423+ElizabathJaison@users.noreply.github.com>
2020-06-04 20:23:52 +0530

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,4 +1,4 @@
-(function () {
+(function (){
   var names = ["Yaakov", "John", "Jen", "Jaison", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
